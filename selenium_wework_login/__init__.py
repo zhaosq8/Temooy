7490c249352263e4fc558c942b7b732993809bbd
zhaosq8 <zhaosq0815@163.com>
2021-12-14 10:45:32 +0800

    ：po 实现

--- selenium_wework_login/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-# @Authon:zhaosq
-# @Time: 2021/12/2 15:02
-# @File: __init__.py.PY
-# @Software: PyCharm
--- selenium_wework_main/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-# @Authon:zhaosq
-# @Time: 2021/12/2 15:57
-# @File: __init__.py.PY
-# @Software: PyCharm
